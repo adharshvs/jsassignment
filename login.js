a114fe384778c42d53ec0506b8b9d2f0b758ac47
Adharsh S <adharshs@qburst.com>
2018-08-03 14:23:22 +0530

    made code changes

--- login.js
+++ login.js
@@ -1,22 +1,11 @@
-var flag = 0;
-
 function login() {
-    var userEmail = document.getElementById('email');
-    var userPass = document.getElementById('pass');
-    
-    users = JSON.parse(localStorage.getItem("users"));    
-    //alert(JSON.stringify(users));
-    for(var user in users) {
-        if(users.hasOwnProperty(user)) {
-            var pass = users[user];
-            if(userEmail.value == user && userPass.value == pass) {                
-                alert("Welcome, " + userEmail.value + ".");
-                document.getElementById("login").reset();
-                flag = 1;                
-            }        
-        } 
-    }
-    if(flag === 0) {
-        alert("Credentials do not match.");
-    }    
+    var userEmail = document.getElementById('email').value;
+    var userPass = document.getElementById('pass').value;
+    if(!localStorage.hasOwnProperty(userEmail)) {
+        alert("No such account exists");
+    } else if(localStorage.getItem(userEmail) == userPass) {
+        alert("Welcome, " + userEmail);
+    } else {
+        alert("Password is wrong.");
+    }     
 }
